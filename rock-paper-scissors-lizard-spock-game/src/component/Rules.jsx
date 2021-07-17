7ef365002d024757d5cf08faecc969348d3997d4
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2021-07-17 18:55:55 +0700

    rule

---
+++ rock-paper-scissors-lizard-spock-game/src/component/Rules.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Rules = () => {
+  return (
+    <div className='fixed bottom-10 right-1/2 lg:right-10 transform translate-x-1/2 lg:translate-x-0'>
+      <button className='text-white border lg:border-2 border-white rounded-lg p-3 font-semibold w-32 lg:w-40 text-sm lg:text-xl tracking-widest'>
+        RULES
+      </button>
+    </div>
+  );
+};
+
+export default Rules;
