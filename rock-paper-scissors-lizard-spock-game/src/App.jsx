3bd323fbdea8750d742e7fa7d1ce6c6d30343af5
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2021-07-17 23:41:50 +0700

    handle pick

--- rock-paper-scissors-lizard-spock-game/src/App.jsx
+++ rock-paper-scissors-lizard-spock-game/src/App.jsx
@@ -1,10 +1,64 @@
-import React, { useState } from 'react'
+import React from "react";
+
+import { AppProvider } from "./context/AppContext";
+
+import Console from "./component/Console";
+import Header from "./component/Header";
+import Rules from "./component/Rules";
+
+const initialState = {
+  playerPick: "",
+  botPick: "",
+  play: false,
+  score: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "pickItem":
+      return { ...state, playerPick: action.inputItem, play: true };
+    case "pickBotItem":
+      return { ...state, botPick: action.inputItem };
+    case "reset":
+      return { playerPick: "", botPick: "", play: false, score: 0 };
+    default:
+      return state;
+  }
+};
+
+const itemName = ["scissors", "spock", "paper", "lizzard", "rock"];
 
 function App() {
+  const [currentState, dispatch] = React.useReducer(reducer, initialState);
+
+  console.log(currentState);
+
+  const getRandomInt = React.useMemo(() => {
+    return Math.floor(Math.random() * 6);
+  }, []);
+
+  const handlePick = React.useCallback((value) => {
+    setTimeout(
+      () =>
+        dispatch({ type: "pickBotItem", inputItem: itemName[getRandomInt] }),
+      1000
+    );
+    dispatch({ type: "pickItem", inputItem: value });
+  }, []);
+
+  const handleReset = React.useCallback(() => {
+    dispatch({ type: "reset" });
+  }, []);
 
   return (
-   <div></div>
-  )
+    <AppProvider value={{ ...currentState, handlePick, handleReset }}>
+      <div className='w-screen h-screen bg-apps-radial bg-top flex flex-col p-8 items-center space-y-10'>
+        <Header />
+        <Console />
+        <Rules />
+      </div>
+    </AppProvider>
+  );
 }
 
-export default App
+export default App;
--- rock-paper-scissors-lizard-spock-game/src/component/Console.jsx
+++ rock-paper-scissors-lizard-spock-game/src/component/Console.jsx
@@ -1,87 +1,96 @@
 import React from "react";
 
+import { AppContext } from "../context/AppContext";
+
 import IcPentagon from "../assets/bg-pentagon.svg";
-import IcScissors from "../assets/icon-scissors.svg";
-import IcRock from "../assets/icon-rock.svg";
-import IcPaper from "../assets/icon-paper.svg";
-import IcLizard from "../assets/icon-lizard.svg";
-import IcSpock from "../assets/icon-spock.svg";
+import ItemButton from "./ItemButton";
+
+import { item } from "../constant/constant";
 
 const Console = () => {
-  const [pick, setPick] = React.useState(false);
+  const { play, handlePick, handleReset, playerPick, botPick } =
+    React.useContext(AppContext);
 
   return (
     <div className='w-full lg:w-3/4 h-full flex flex-col justify-start lg:justify-center '>
-      {!pick ? (
+      {!play ? (
         <div className='flex items-center justify-center'>
-          <div className='relative  bg-green-400'>
+          <div className='relative'>
             <img
               src={IcPentagon}
               alt='pentagon background'
               className='h-3/4 lg:h-full'
             />
-            <button
-              onClick={() => setPick(true)}
-              className='flex items-center justify-center absolute w-1/3 h-1/3 lg:w-1/2 lg:h-1/2 rounded-full bg-gradient-to-b from-yellow-2 to-yellow-1 top-0 left-1/2 transform -translate-x-1/2 lg:-translate-y-1/2'
-            >
-              <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-white border-t-6 border-gray-300'>
-                <img src={IcScissors} alt='scissors' className='h-1/2' />
-              </div>
-            </button>
-            <div className='flex items-center justify-center absolute w-1/3 h-1/3 lg:w-1/2 lg:h-1/2 rounded-full bg-gradient-to-b from-cyan-2 to-cyan-1 top-0 left-0 transform lg:-translate-x-1/2 translate-y-3/4 lg:translate-y-1/4'>
-              <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-white border-t-6 border-gray-300'>
-                <img src={IcSpock} alt='scissors' className='h-1/2' />
-              </div>
-            </div>
-            <div className='flex items-center justify-center absolute w-1/3 h-1/3 lg:w-1/2 lg:h-1/2 rounded-full bg-gradient-to-b from-blue-2 to-blue-1 top-0 right-0 transform lg:translate-x-1/2 translate-y-3/4 lg:translate-y-1/4'>
-              <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-white border-t-6 border-gray-300'>
-                <img src={IcPaper} alt='scissors' className='h-1/2' />
-              </div>
-            </div>
-            <div className='flex items-center justify-center absolute w-1/3 h-1/3 lg:w-1/2 lg:h-1/2 rounded-full bg-gradient-to-b from-purple-2 to-purple-1 bottom-0 left-0 transform translate-x-1/4 lg:translate-y-1/2 lg:-translate-x-1/4'>
-              <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-white border-t-6 border-gray-300'>
-                <img src={IcLizard} alt='scissors' className='h-1/2' />
-              </div>
-            </div>
-            <div className='flex items-center justify-center absolute w-1/3 h-1/3 lg:w-1/2 lg:h-1/2 rounded-full bg-gradient-to-b from-red-2 to-red-1 bottom-0 right-0 transform -translate-x-1/4 lg:translate-y-1/2 lg:translate-x-1/4'>
-              <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-white border-t-6 border-gray-300'>
-                <img src={IcRock} alt='scissors' className='h-1/2' />
-              </div>
-            </div>
+            {Object.keys(item).map((key) => (
+              <ItemButton
+                key={item[key]["name"]}
+                func={() => handlePick(item[key]["name"])}
+                iconSrc={item[key]["icon"]}
+                iconAlt={item[key]["name"]}
+                fill={item[key]["fillStr"]}
+                position={item[key]["positionStr"]}
+              />
+            ))}
           </div>
         </div>
       ) : (
         <div className='relative flex w-full justify-evenly items-start lg:items-center  h-3/4 lg:h-auto'>
           <div className='flex flex-col lg:flex-col-reverse space-y-5 lg:space-y-16 lg:space-y-reverse items-center'>
-            <button
-              onClick={() => setPick(true)}
-              className='flex items-center justify-center w-32 h-32 lg:w-64 lg:h-64 rounded-full bg-gradient-to-b from-yellow-2 to-yellow-1'
+            <div
+              className={`flex items-center justify-center w-32 h-32 lg:w-64 lg:h-64 rounded-full bg-gradient-to-b ${item[playerPick]["fillStr"]}`}
             >
               <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-white border-t-6 border-gray-300'>
-                <img src={IcScissors} alt='scissors' className='h-1/2' />
+                <img
+                  src={item[playerPick]["icon"]}
+                  alt={playerPick}
+                  className='h-1/2'
+                />
               </div>
-            </button>
-            <p className='text-white text-2xl font-bold'>YOU PICKED</p>
+            </div>
+            <p className='text-white text-sm lg:text-2xl font-bold'>
+              YOU PICKED
+            </p>
           </div>
           <div className='absolute lg:static left-1/2 top-2/3 transform -translate-x-1/2 lg:transform-none space-y-3'>
             <p className=' text-6xl whitespace-nowrap font-semibold text-white'>
               YOU WIN
             </p>
-            <button className='bg-white rounded-lg p-4 w-full'>
+            <button
+              onClick={() => handleReset()}
+              className='bg-white rounded-lg p-4 w-full'
+            >
               PLAY AGAIN
             </button>
           </div>
-          <div className='flex flex-col lg:flex-col-reverse space-y-5 lg:space-y-16 lg:space-y-reverse items-center'>
-            <button
-              onClick={() => setPick(true)}
-              className='flex items-center justify-center w-32 h-32 lg:w-64 lg:h-64 rounded-full bg-gradient-to-b from-yellow-2 to-yellow-1'
-            >
-              <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-white border-t-6 border-gray-300'>
-                <img src={IcScissors} alt='scissors' className='h-1/2' />
+          {botPick != "" ? (
+            <div className='flex flex-col lg:flex-col-reverse space-y-5 lg:space-y-16 lg:space-y-reverse items-center'>
+              <div
+                className={`flex items-center justify-center w-32 h-32 lg:w-64 lg:h-64 rounded-full bg-gradient-to-b ${item[playerPick]["fillStr"]}`}
+              >
+                <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-white border-t-6 border-gray-300'>
+                  <img
+                    src={item[botPick]["icon"]}
+                    alt={botPick}
+                    className='h-1/2'
+                  />
+                </div>
               </div>
-            </button>
-            <p className='text-white text-2xl font-bold'>YOU PICKED</p>
-          </div>
+              <p className='text-white text-sm lg:text-2xl font-bold'>
+                THE HOUSE PICKED
+              </p>
+            </div>
+          ) : (
+            <div className='flex flex-col lg:flex-col-reverse space-y-5 lg:space-y-16 lg:space-y-reverse items-center'>
+              <div
+                className={`flex items-center justify-center w-32 h-32 lg:w-64 lg:h-64 rounded-full`}
+              >
+                <div className='flex items-center justify-center w-3/4 h-3/4 rounded-full bg-screen-2 bg-opacity-30'></div>
+              </div>
+              <p className='text-white text-sm lg:text-2xl font-bold'>
+                THE HOUSE PICKED
+              </p>
+            </div>
+          )}
         </div>
       )}
     </div>
