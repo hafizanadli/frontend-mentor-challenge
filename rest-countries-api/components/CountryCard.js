a6dc6af9e1f741851ec86049691f75ca31674731
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2021-08-03 00:32:35 +0700

    add country card

--- rest-countries-api/components/CountryCard.js
+++ rest-countries-api/components/CountryCard.js
@@ -1,31 +1,47 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const CountryCard = () => {
+const CountryCard = ({
+  countryName,
+  flagImg,
+  population,
+  capital,
+  region,
+  callingCodes,
+}) => {
   return (
-    <div className='text-left rounded-lg bg-element-responsive'>
-      <div className='relative h-44'>
-        <Image
-          alt='country'
-          src='https://restcountries.eu/data/afg.svg'
-          layout='fill'
-          objectFit='fill'
-          quality={75}
-          className='rounded-t-lg'
-        />
+    <Link
+      href={{
+        pathname: "/country/[codes]/[country]",
+        query: { codes: callingCodes, country: countryName },
+      }}
+    >
+      <div className='text-left rounded-lg bg-element-responsive'>
+        <div className='relative h-52'>
+          <Image
+            alt='country'
+            src={flagImg}
+            layout='fill'
+            objectFit='cover'
+            quality={75}
+            className='rounded-t-lg'
+          />
+        </div>
+        <div className='px-8 pt-8 pb-12'>
+          <h3 className='mb-5'>{countryName}</h3>
+          <p className='text-sm mb-2'>
+            <span className='font-semibold'>Population:</span>{" "}
+            {population.toLocaleString("en-US")}
+          </p>
+          <p className='text-sm mb-2'>
+            <span className='font-semibold'>Region:</span> {region}
+          </p>
+          <p className='text-sm'>
+            <span className='font-semibold'>Capital:</span> {capital}
+          </p>
+        </div>
       </div>
-      <div className='px-8 pt-8 pb-14'>
-        <h3 className='mb-5'>Germany</h3>
-        <p className='text-sm'>
-          <span className='font-semibold'>Population:</span> 81,700,000
-        </p>
-        <p>
-          <span className='font-semibold'>Region:</span> Europe
-        </p>
-        <p>
-          <span className='font-semibold'>Capital:</span> Berlin
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- rest-countries-api/pages/index.js
+++ rest-countries-api/pages/index.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import CountryCard from "../components/CountryCard";
 
-export default function Home() {
+export async function getStaticProps() {
+  const getCountryData = async () => {
+    const res = await fetch(
+      "https://restcountries.eu/rest/v2/regionalbloc/asean",
+      {
+        method: "GET",
+      }
+    );
+    return await res.json();
+  };
+  const countryData = await getCountryData();
+  return {
+    props: {
+      countryList: countryData,
+    },
+  };
+}
+
+export default function Home({ countryList }) {
+  const [test, setTest] = useState(false);
+  console.log(countryList);
+
   return (
     <div className='flex-1 flex flex-col w-full px-20 mt-16'>
       <Head>
@@ -9,13 +31,21 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16 sm:w-full '>
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-24 sm:w-full '>
+        {countryList.map((el) => (
+          <CountryCard
+            key={el.alpha3Code}
+            callingCodes={el.callingCodes}
+            capital={el.capital}
+            countryName={el.name}
+            flagImg={el.flag}
+            population={el.population}
+            region={el.region}
+          />
+        ))}
       </div>
+
+      <p>dsadsa</p>
 
       {/* <footer className='flex items-center justify-center w-full h-24 border-t bg-yellow-500'>
       </footer> */}
