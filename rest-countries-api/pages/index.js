15ca25c83e5e70fc5c4db65a9b21f7a3b62b440d
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2021-08-10 00:51:13 +0700

    get country by region

--- rest-countries-api/pages/index.js
+++ rest-countries-api/pages/index.js
@@ -1,35 +1,50 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import Head from "next/head";
 import CountryCard from "../components/CountryCard";
+import Filter from "../components/Filter";
 
 export async function getStaticProps() {
   const getCountryData = async () => {
-    const res = await fetch(
-      "https://restcountries.eu/rest/v2/regionalbloc/asean",
-      {
-        method: "GET",
-      }
-    );
+    const res = await fetch("https://restcountries.eu/rest/v2/all", {
+      method: "GET",
+    });
     return await res.json();
   };
   const countryData = await getCountryData();
   return {
     props: {
-      countryList: countryData,
+      countryData: countryData,
     },
   };
 }
 
-export default function Home({ countryList }) {
-  const [test, setTest] = useState(false);
-  console.log(countryList);
+export default function Home({ countryData }) {
+  const allCountryRef = useRef(countryData);
+  const [countryList, setCountryList] = useState(countryData);
+
+  const getCountryByRegion = useCallback(async ({ regionCode }) => {
+    const res = await fetch(
+      "https://restcountries.eu/rest/v2/region/" + regionCode,
+      {
+        method: "GET",
+      }
+    );
+    const countryData = await res.json();
+    setCountryList(countryData);
+  }, []);
 
   return (
-    <div className='flex-1 flex flex-col w-full px-20 mt-16'>
+    <div className='flex-1 flex flex-col w-full px-20 mt-16 space-y-16'>
       <Head>
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <Filter
+        getCountryByRegion={getCountryByRegion}
+        setCountryList={setCountryList}
+        allCountryRef={allCountryRef}
+      />
 
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-24 sm:w-full '>
         {countryList.map((el) => (
@@ -44,8 +59,6 @@ export default function Home({ countryList }) {
           />
         ))}
       </div>
-
-      <p>dsadsa</p>
 
       {/* <footer className='flex items-center justify-center w-full h-24 border-t bg-yellow-500'>
       </footer> */}
