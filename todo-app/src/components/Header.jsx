7080b5b18529f89512bb45e250e4b1654b228a84
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2021-08-18 21:34:16 +0700

    edit style

--- todo-app/src/components/Header.jsx
+++ todo-app/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ bgHeader }) => {
   return (
     <div
-      className='absolute top-0 left-0 w-full bg-cover h-64'
+      className='absolute top-0 left-0 w-full bg-cover h-72 md:h-96'
       style={{
         backgroundImage: `url(${bgHeader})`,
       }}
