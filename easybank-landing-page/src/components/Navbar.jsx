f734493d78730fd7aba82794525e163539935f69
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2021-08-22 13:16:19 +0700

    edit container

--- easybank-landing-page/src/components/Navbar.jsx
+++ easybank-landing-page/src/components/Navbar.jsx
@@ -4,22 +4,20 @@ import Logo from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <header className='fixed top-0 w-full outer-container bg-white z-50'>
-      <div className='inner-container flex justify-between items-center mx-auto h-full my-6 px-5'>
-        <img src={Logo} alt='easybank logo' className='h-5' />
-        <ul className='nav-menu flex-1 justify-center items-center space-x-5 text-grayishBlue hidden md:flex'>
-          <li>
-            <a href=''>Home</a>
-          </li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Blog</li>
-          <li>Career</li>
-        </ul>
-        <button className='rounded-[1.2rem] bg-gradient-to-r from-limeGreen  to-brightCyan py-3 px-10 text-white hidden md:block'>
-          Request Invite
-        </button>
-      </div>
+    <header className='fixed top-0 w-full bg-white z-50 flex justify-between items-center py-5 px-5 lg:px-32'>
+      <img src={Logo} alt='easybank logo' className='h-5' />
+      <ul className='nav-menu flex-1 justify-center items-center space-x-5 text-grayishBlue hidden md:flex'>
+        <li>
+          <a href=''>Home</a>
+        </li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Blog</li>
+        <li>Career</li>
+      </ul>
+      <button className='rounded-[1.2rem] bg-gradient-to-r from-limeGreen  to-brightCyan py-3 px-10 text-white hidden md:block'>
+        Request Invite
+      </button>
     </header>
   );
 };
