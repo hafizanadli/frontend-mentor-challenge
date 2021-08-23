2a2989307b921a7fa05adfe1fa0753d307435ce0
HAFIZAN ADLIRACHMAN <adli.hafizan893@gmail.com>
2021-08-23 23:23:18 +0700

    mobile menu

--- easybank-landing-page/src/App.jsx
+++ easybank-landing-page/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -10,13 +10,19 @@ import MockupBg from "./assets/image-mockups.png";
 import PatternDesktop from "./assets/bg-intro-desktop.svg";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main className='relative'>
-      <Navbar />
+      <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
       <Section1 />
       <Section2 />
       <Section3 />
       <Footer />
+      <div
+        className={`absolute bg-gradient-to-b from-black  to-white ${
+          isOpen ? "opacity-75 w-full" : "opacity-0 w-0"
+        }  h-full top-0 transition duration-300 ease-in-out`}
+      ></div>
       <div className='flex'>
         <div className='w-10 h-10 bg-darkBlue' />
         <div className='w-10 h-10 bg-limeGreen' />
--- easybank-landing-page/src/components/Navbar.jsx
+++ easybank-landing-page/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Logo from "../assets/logo.svg";
+import IcClose from "../assets/icon-close.svg";
+import IcMenu from "../assets/icon-hamburger.svg";
 
 const nav = [
   { title: "Home" },
@@ -10,13 +12,16 @@ const nav = [
   { title: "Career" },
 ];
 
-const Navbar = () => {
+const Navbar = ({ isOpen, setIsOpen }) => {
   return (
     <header className='fixed top-0 w-full bg-white z-50 flex justify-between items-center px-5 lg:px-32 py-5 md:py-0'>
       <img src={Logo} alt='easybank logo' className='h-5' />
       <ul className='nav-menu flex-1 justify-center items-center space-x-5 text-grayishBlue hidden md:flex h-20'>
         {nav.map((el, index) => (
-          <li className='relative h-full flex flex-col justify-center'>
+          <li
+            className='relative h-full flex flex-col justify-center'
+            key={index}
+          >
             <a href='#' className='hover:text-darkBlue peer'>
               {el.title}
             </a>
@@ -27,6 +32,27 @@ const Navbar = () => {
       <button className='rounded-[1.2rem] bg-gradient-to-r from-limeGreen  to-brightCyan py-3 px-10 text-white hidden md:block hover:opacity-80'>
         Request Invite
       </button>
+      <div
+        className='md:hidden block cursor-pointer'
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <img src={isOpen ? IcClose : IcMenu} alt='menu' />
+      </div>
+      <div
+        className={`absolute ${
+          isOpen ? "top-20 opacity-100" : "-top-56 opacity-0"
+        } z-20 w-[90vw] left-[50%] transform -translate-x-1/2 rounded-md bg-white p-5 text-center transition-all duration-300 ease-in-out`}
+      >
+        <ul className='space-y-3'>
+          {nav.map((el, index) => (
+            <li className='' key={index}>
+              <a href='#' className=''>
+                {el.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 };
